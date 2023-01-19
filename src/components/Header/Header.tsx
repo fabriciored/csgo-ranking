86aba4aa2a2990a942d602045b6ea51285767bc3
fabriciored <fabriciojlsantos@gmail.com>
2023-01-19 00:33:29 -0300

    feat: create header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import styles from '@/styles/Header.module.css'
+
+export default function Header() {
+return(
+    <div className={styles.header}>
+        <h1>CSGORanking</h1>
+        <a href="https://fabriciodev.me"><h1>Visit my website</h1></a>
+    </div>
+)
+}
