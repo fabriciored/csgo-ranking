163872497f239d526a1f085a47d8bc0ef1085e8e
fabriciored <fabriciojlsantos@gmail.com>
2023-01-20 03:54:58 -0300

    style: change link to website style

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,12 @@
 import styles from '@/styles/Header.module.css'
 
+import { HiArrowTopRightOnSquare } from 'react-icons/hi2';
+
 export default function Header() {
 return(
     <div className={styles.header}>
         <h1>CSGORanking</h1>
-        <a href="https://fabriciodev.me"><h1>Visit my website</h1></a>
+        <a href="https://fabriciodev.me" target={'_blank'}><h3 className={styles.visitMyWebsite}>Visit my website <HiArrowTopRightOnSquare/></h3></a>
     </div>
 )
 }
