7d25ca60017a742a718daad6e298c62fc468c449
fabriciored <fabriciojlsantos@gmail.com>
2023-01-21 04:54:30 -0300

    feat && style: create navbar component

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,25 @@
+import styles from '@/styles/Navbar.module.css'
+import Link from 'next/link'
+import { GiTrophy, GiCrossedSwords } from 'react-icons/gi';
+import { AiFillHome } from 'react-icons/ai'
+import { MdEventNote } from 'react-icons/md'
+
+
+export default function Navbar() {
+    return (
+        <div className={styles.navbar}>
+        <Link href="/">
+          <p className={styles.navItem}><AiFillHome/> Home</p>
+        </Link>
+        <Link href="/top-teams">
+          <p className={styles.navItem}><GiTrophy/> Top Teams</p>
+        </Link>
+        <Link href="/events">
+          <p className={styles.navItem}><GiCrossedSwords/> Matches</p>
+        </Link>
+        <Link href="/matches">
+          <p className={styles.navItem}><MdEventNote/> Events</p>
+        </Link>
+    </div>
+    )
+}
