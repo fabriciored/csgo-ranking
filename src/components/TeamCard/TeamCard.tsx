5b9a2618376d66d8d3fa718374427b580094cc1a
fabriciored <fabriciojlsantos@gmail.com>
2023-01-23 21:35:37 -0300

    feat: add 'minimal' option to player and playercard components

--- src/components/TeamCard/TeamCard.tsx
+++ src/components/TeamCard/TeamCard.tsx
@@ -3,36 +3,98 @@ import styles from "@/styles/TeamCard.module.css";
 
 interface teamProps {
   rank: string;
-  teamName: string,
-  logoUrl: string,
-  player_picture: [string, string, string, string, string],
-  player_name: [string, string, string, string, string],
-  player_flag: [string, string, string, string, string],
+  teamName: string;
+  logoUrl: string;
+  player_picture: [string, string, string, string, string];
+  player_name: [string, string, string, string, string];
+  player_flag: [string, string, string, string, string];
+  minimal?: boolean;
 }
 
 export default function TeamCard(props: teamProps) {
   return (
     <div className={styles.card}>
       <div className={styles.upper}>
-        <div className={styles.left}>
-          {
-          props.rank == '#1'? <h2 className={styles.rank1}>{props.rank}</h2> : props.rank == '#2' ? <h2 className={styles.rank2}>{props.rank}</h2> : props.rank == '#3' ? <h2 className={styles.rank3}>{props.rank}</h2> : <h2 className={styles.rank}>{props.rank}</h2> 
-          }
-          <img className={styles.logo} src={props.logoUrl} alt="" />
+        <div className={!props.minimal ? styles.left : styles.leftMinimal}>
+          {props.rank == "#1" ? (
+            <h2 className={styles.rank1}>{props.rank}</h2>
+          ) : props.rank == "#2" ? (
+            <h2 className={styles.rank2}>{props.rank}</h2>
+          ) : props.rank == "#3" ? (
+            <h2 className={styles.rank3}>{props.rank}</h2>
+          ) : (
+            <h2 className={styles.rank}>{props.rank}</h2>
+          )}
+          <img className={!props.minimal ? styles.logo : styles.logoMinimal} src={props.logoUrl} alt="" />
         </div>
-        <div className={styles.right}>
-        <h2 className={styles.name}>{props.teamName}</h2>
+        <div className={!props.minimal ? styles.right : styles.rightMinimal}>
+          <h2 className={styles.name}>{props.teamName}</h2>
         </div>
       </div>
 
       <div className={styles.lower}>
-
-      <Player picture={props.player_picture[0]} name={props.player_name[0]} flag={props.player_flag[0]}></Player>
-      <Player picture={props.player_picture[1]} name={props.player_name[1]} flag={props.player_flag[1]}></Player>
-      <Player picture={props.player_picture[2]} name={props.player_name[2]} flag={props.player_flag[2]}></Player>
-      <Player picture={props.player_picture[3]} name={props.player_name[3]} flag={props.player_flag[3]}></Player>
-      <Player picture={props.player_picture[4]} name={props.player_name[4]} flag={props.player_flag[4]}></Player>
-
+        {!props.minimal ? (
+          <>
+            <Player
+              picture={props.player_picture[0]}
+              name={props.player_name[0]}
+              flag={props.player_flag[0]}
+            ></Player>
+            <Player
+              picture={props.player_picture[1]}
+              name={props.player_name[1]}
+              flag={props.player_flag[1]}
+            ></Player>
+            <Player
+              picture={props.player_picture[2]}
+              name={props.player_name[2]}
+              flag={props.player_flag[2]}
+            ></Player>
+            <Player
+              picture={props.player_picture[3]}
+              name={props.player_name[3]}
+              flag={props.player_flag[3]}
+            ></Player>
+            <Player
+              picture={props.player_picture[4]}
+              name={props.player_name[4]}
+              flag={props.player_flag[4]}
+            ></Player>
+          </>
+        ) : (
+          <>
+            <Player
+              picture={props.player_picture[0]}
+              name={props.player_name[0]}
+              flag={props.player_flag[0]}
+              minimal
+            ></Player>
+            <Player
+              picture={props.player_picture[1]}
+              name={props.player_name[1]}
+              flag={props.player_flag[1]}
+              minimal
+            ></Player>
+            <Player
+              picture={props.player_picture[2]}
+              name={props.player_name[2]}
+              flag={props.player_flag[2]}
+              minimal
+            ></Player>
+            <Player
+              picture={props.player_picture[3]}
+              name={props.player_name[3]}
+              flag={props.player_flag[3]}
+              minimal
+            ></Player>
+            <Player
+              picture={props.player_picture[4]}
+              name={props.player_name[4]}
+              flag={props.player_flag[4]}
+              minimal
+            ></Player>
+          </>
+        )}
       </div>
     </div>
   );
--- src/elements/Player/Player.tsx
+++ src/elements/Player/Player.tsx
@@ -3,15 +3,18 @@ import styles from "@/styles/Player.module.css";
 interface playerProps {
     picture: string,
     name: string,
-    flag: string
+    flag: string,
+    minimal?: boolean
 }
 
 export default function Player(props: playerProps) {
     return (
     <>
         <div className={styles.player}>
-        <img className={styles.playerImage} src={props.picture} alt={props.name} />
-        <div className={styles.nameAndFlag}>
+        <img className={!props.minimal ? styles.playerImage
+        : styles.playerImageMinimal} src={props.picture} alt={props.name} />
+        <div className={!props.minimal ? styles.nameAndFlag
+        : styles.nameAndFlagMinimal}>
         <h4>{props.name}</h4>
         <img src={props.flag}/>
         </div>
